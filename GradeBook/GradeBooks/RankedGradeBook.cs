347a28c89ea5f14da68daf28e253fa2748582003
JSetlik <jakub.setlik@microsoft.wsei.edu.pl>
2022-04-03 20:21:32 +0200

    10/24

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -9,12 +9,14 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
 
+
         }
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
             {
                 throw new InvalidOperationException("Students number less than 5");
+
             }
             if (averageGrade >= 80)
                 return 'A';
@@ -27,6 +29,18 @@ namespace GradeBook.GradeBooks
             else
                 return 'F';
         }
+
+
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else base.CalculateStatistics();
+        }
+
 
 
     }
