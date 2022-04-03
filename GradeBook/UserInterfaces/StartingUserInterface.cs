ae1022a4f16ecbe2a21fbc09e0305a28e5149fd5
JSetlik <jakub.setlik@microsoft.wsei.edu.pl>
2022-04-03 20:30:35 +0200

    13/24

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,11 +34,13 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
-            {
-                Console.WriteLine("Command not valid, Create requires a name.");
+            
+                if (parts.Length != 3)
+                {
+                   
+                    Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
-            }
+                }
             var name = parts[1];
             BaseGradeBook gradeBook = new BaseGradeBook(name);
             Console.WriteLine("Created gradebook {0}.", name);
