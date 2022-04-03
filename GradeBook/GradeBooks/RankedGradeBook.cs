940e08013e75c0786536c1f14155e458d57215cb
JSetlik <jakub.setlik@microsoft.wsei.edu.pl>
2022-04-03 20:16:50 +0200

    9/24

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,24 @@ namespace GradeBook.GradeBooks
         {
 
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Students number less than 5");
+            }
+            if (averageGrade >= 80)
+                return 'A';
+            else if (averageGrade < 80 && averageGrade >= 60)
+                return 'B';
+            else if (averageGrade >= 40 && averageGrade < 60)
+                return 'C';
+            else if (averageGrade >= 20 && averageGrade < 40)
+                return 'D';
+            else
+                return 'F';
+        }
+
+
     }
 }
