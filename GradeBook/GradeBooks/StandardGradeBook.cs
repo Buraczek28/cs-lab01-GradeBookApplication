21c567ce19f519dfbb31fa67751d00e7a451cd67
JSetlik <jakub.setlik@microsoft.wsei.edu.pl>
2022-04-03 20:09:42 +0200

    3/24

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+
+
+
+    public class StandardGradeBook : BaseGradeBook
+    {
+
+
+        public StandardGradeBook(string name) : base(name)
+        {
+
+        }
+    }
+
+
+
+
+}
